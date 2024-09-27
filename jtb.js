f46d81062c4892dd06cd4c90cdb908caedb6ecea
hydrophobis <142858559+hydrophobis@users.noreply.github.com>
2024-09-27 13:50:35 +0000

    Should work now lmfao

--- jtb.js
+++
@@ -1,4 +0,0 @@
-game = document.appendChild('script');
-game.src = 'main.js';
-
-Gamepad.TieredAchievement('Test', 'Hello', 'Cursor', 1)
--- jtba.js
+++
@@ -1,2 +0,0 @@
-Gamepad.TieredAchievement('Test', 'Hello', 'Cursor', 1)
-new Game.Achievement('Click2','Have <b>1</b> cursor.',[0,0]);
